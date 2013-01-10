219c75eefe5804b7faab5c5c6275a62138096189
romanalesenkov <karlushka_15@tut.by>
2013-01-10 20:40:39 +0300

    calculation

--- lab_1_2/lab_1_2.php
+++ lab_1_2/lab_1_2.php
@@ -31,4 +31,31 @@ $num_tarif = $i;
 //отделение минут от секунд
 for ($i=0; $i<$num_history; $i++)
 	$MM_SS[$i] = explode(":", $A[$i]['prodolzh'], 2);
+$the_best = 99999999;
+//расчет расходов
+for($j=0; $j<$num_tarif; $j++) {
+	for ($i=0; $i<$num_history; $i++) {
+		if($A[$i]['napravl'] == 'out') {
+			if($A[$i]['tip'] == 'sms') {
+				if($B[$j]['sum_bespl_sms']>0) 
+					$B[$j]['sum_bespl_sms']--;
+				else $sum_sms[$j]++;
+			}
+			else {
+				if($B[$j]['sum_bespl_voice']>0)
+					$B[$j]['sum_bespl_voice'] -= $MM_SS[$i][0]+1;
+				else {
+					$number[$j]++;
+					$sum_voice[$j] += $MM_SS[$i][0]+1;
+				}
+			}
+		}
+	$sum[$j] = $sum_sms[$j]*$B[$j]['cost_sms'] - $B[$j]['sum_bespl_voice']*$B[$j]['cost_voice'] + ($sum_voice[$j]-$number[$j])*$B[$j]['cost_voice'] + $number[$j]*$B[$j]['cost_voice_1']+$B[$j]['abon_plata'];
+	}	
+	//echo $sum[$j]."=sum[$j] <br>";
+    if($the_best > $sum[$j]) {
+		$the_best = $sum[$j];
+		$k=$j;
+	}
+}
 ?>
